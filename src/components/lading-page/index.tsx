66802193252721b84f252e2cb1074c87cb119cc3
malik <malik.shaik@outlook.com>
2022-08-04 23:37:54 +0200

    updated landing page

--- src/components/lading-page/index.tsx
+++ src/components/lading-page/index.tsx
@@ -1,5 +1,32 @@
-import React from 'react'
+import { FC } from 'react'
+import { Box, createStyles, makeStyles, Theme } from '@material-ui/core'
+import { Search } from 'components/lading-page/search'
+import { Results } from 'components/lading-page/results'
+import { useSearchProducts } from 'lib/use-search-products'
 
-export const LandingPage = () => {
-  return <div>LandingPage</div>
+export const LandingPage: FC = () => {
+  const classes = useStyles()
+  const { searchProducts, loading, error, products } = useSearchProducts()
+
+  return (
+    <Box className={classes.container}>
+      <Search searchProducts={searchProducts} />
+      <Results products={products} loading={loading} error={error} />
+    </Box>
+  )
 }
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    container: {
+      padding: '3%',
+      margin: 'auto',
+      width: '90%',
+      display: 'flex',
+      flexDirection: 'row',
+      [theme.breakpoints.down('sm')]: {
+        flexDirection: 'column',
+      },
+    },
+  })
+)
